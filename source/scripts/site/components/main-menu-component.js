f504d4ce3091c11d6ec6d2877eca3e92ef96d0e2
Diego <diegoalbert89@gmail.com>
2017-05-05 18:09:49 +0200

    MAIN_MENU: Basic functionality to main menu

--- source/scripts/site/components/main-menu-component.js
+++ source/scripts/site/components/main-menu-component.js
@@ -14,12 +14,15 @@ site.components.MainMenuComponent = el.core.utils.class.extend(function(options)
   this.id = el.core.utils.uniqueId.get(this.name + '-');
 
   this.$el = this.options.$el;
+  this.$mainMenuBar = this.$el.find('.main-menu-bar');
+  this.menuOpened = false;
 
   this._register();
 
   console.log(this.name, this.options);
 
   el.core.events.globalDispatcher.on(el.core.events.event.RESIZE, $.proxy(this._resizeMainMenu, this));
+  this.$el.on('click', $.proxy(this._triggerMenuPosition, this))
 
 }, site.components.BaseComponent);
 
@@ -29,6 +32,24 @@ site.components.MainMenuComponent.prototype._resizeMainMenu = function(data) {
 	this.$el.css({
 		'height' : data.height
 	})
+}
+
+site.components.MainMenuComponent.prototype._triggerMenuPosition = function(e) {
+
+	that = this;
+
+	if (this.menuOpened) {
+
+		this.$mainMenuBar.removeClass('active').find('.hamburger-icon').removeClass('open');
+		this.menuOpened = false;
+
+	} else {
+
+		this.$mainMenuBar.addClass('active').find('.hamburger-icon').addClass('open');
+		this.menuOpened = true;
+
+	}
+
 }
 
 site.components.MainMenuComponent.prototype.destroy = function() {
