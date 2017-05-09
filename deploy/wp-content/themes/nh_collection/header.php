6cb8d0ec99447a4adbcbfdba6a106d5c80b23c1d
Diego <diegoalbert89@gmail.com>
2017-05-09 18:46:47 +0200

    HEADER_MAIN_MENU: organize styles for vertical button & make functionallity for the menu in different views

--- deploy/wp-content/themes/nh_collection/header.php
+++ deploy/wp-content/themes/nh_collection/header.php
@@ -1,3 +1,7 @@
+<?php
+    global $post;
+    $post_slug=$post->post_name;
+?>
 <!DOCTYPE html>
 <html lang="<?php bloginfo('language'); ?>">
 <head>
@@ -9,103 +13,110 @@
 
 <body id="nh-collection">
 
-  <nav class="main-menu"
-  data-component="main-menu-component">
+  <div id="page">
 
-    <div class="main-menu-wrapper">
+    <div id="nh-main-content">
 
-      <a href="<?php echo get_home_url(); ?>" class="go-home vertical-button inverted">
-        <span class="icon-home-icon"></span>
-      </a>
-      <div class="main-menu-bar vertical-button">
+    <nav class="main-menu <?php echo $post_slug; ?>"
+      data-page-view="<?php echo $post_slug; ?>"
+      data-component="main-menu-component"
+    >
 
-        <div class="hamburger-icon">
-          <span class="line line-1"></span>
-          <span class="line line-2"></span>
-          <span class="line line-3"></span>
-          <span class="line line-4"></span>
+      <div class="main-menu-wrapper">
+
+        <a href="<?php echo get_home_url(); ?>" class="go-home vertical-button inverted">
+          <span class="icon-home-icon"></span>
+        </a>
+        <div class="main-menu-bar vertical-button">
+
+          <div class="hamburger-icon">
+            <span class="line line-1"></span>
+            <span class="line line-2"></span>
+            <span class="line line-3"></span>
+            <span class="line line-4"></span>
+          </div>
+
+        </div>
+
+        <div class="section-wrapper">
+          <a href="#"> <!-- link to collectors -->
+            <span class="section-item section-parent">
+                <div class="overlay-element white">
+                  <div class="relative-wrapper">
+                    <div class="img-wrapper">
+                      <img src="<?php echo get_template_directory_uri() . '/assets/images/main-menu/collectors.jpg'?>" alt="the collectors">
+                    </div>
+                    <div class="header-wrapper white">
+                      <p class="navigation--header small"><span class="pre">The</span><br />feelling<br /> collectors</p>
+                    </div>
+                  </div>
+                </div>
+            </span>
+          </a>
+
+          <a href="#"> <!-- link to experiences -->
+            <span class="section-item section-parent">
+              <div class="overlay-element white">
+                <div class="relative-wrapper">
+                  <div class="img-wrapper">
+                    <img src="<?php echo get_template_directory_uri() . '/assets/images/main-menu/experiences.jpg'?>" alt="the experiences">
+                  </div>
+                  <div class="header-wrapper white">
+                    <p class="navigation--header small"><span class="pre no-space">The</span><br />experiences</p>
+                  </div>
+                </div>
+              </div>
+            </span>
+          </a>
+
+          <a href="#"> <!-- link to feels -->
+            <span class="section-item section-parent">
+              <div class="overlay-element white">
+                <div class="relative-wrapper">
+                  <div class="img-wrapper">
+                    <img src="<?php echo get_template_directory_uri() . '/assets/images/main-menu/feels.jpg'?>" alt="the feels">
+                  </div>
+                  <div class="header-wrapper white">
+                    <p class="navigation--header small"><span class="pre">The</span><br />feels</p>
+                  </div>
+                </div>
+              </div>
+            </span>
+          </a>
+
+          <a href="#"> <!-- link to dictionary -->
+            <span class="section-item section-parent">
+              <div class="overlay-element white">
+                <div class="relative-wrapper">
+                  <div class="img-wrapper">
+                    <img src="<?php echo get_template_directory_uri() . '/assets/images/main-menu/dictionary.jpg'?>" alt="the dictionary">
+                  </div>
+                  <div class="header-wrapper white">
+                    <p class="navigation--header small"><span class="pre">The</span><br />dictionary</p>
+                  </div>
+                </div>
+              </div>
+            </span>
+          </a>
+
+          <a href="#"> <!-- link to voyages -->
+            <span class="section-item section-parent">
+              <div class="overlay-element white">
+                <div class="relative-wrapper">
+                  <div class="img-wrapper">
+                    <img src="<?php echo get_template_directory_uri() . '/assets/images/main-menu/voyages.jpg'?>" alt="the voyages">
+                  </div>
+                  <div class="header-wrapper white">
+                    <p class="navigation--header small"><span class="pre no-space">The</span><br />voyages</p>
+                  </div>
+                </div>
+              </div>
+            </span>
+          </a>
+
         </div>
 
       </div>
 
-      <div class="section-wrapper">
-        <a href="#"> <!-- link to collectors -->
-          <span class="section-item section-parent">
-              <div class="overlay-element white">
-                <div class="relative-wrapper">
-                  <div class="img-wrapper">
-                    <img src="<?php echo get_template_directory_uri() . '/assets/images/main-menu/collectors.jpg'?>" alt="the collectors">
-                  </div>
-                  <div class="header-wrapper white">
-                    <p class="navigation--header small"><span class="pre">The</span><br />feelling<br /> collectors</p>
-                  </div>
-                </div>
-              </div>
-          </span>
-        </a>
+    </nav>
 
-        <a href="#"> <!-- link to experiences -->
-          <span class="section-item section-parent">
-            <div class="overlay-element white">
-              <div class="relative-wrapper">
-                <div class="img-wrapper">
-                  <img src="<?php echo get_template_directory_uri() . '/assets/images/main-menu/experiences.jpg'?>" alt="the experiences">
-                </div>
-                <div class="header-wrapper white">
-                  <p class="navigation--header small"><span class="pre no-space">The</span><br />experiences</p>
-                </div>
-              </div>
-            </div>
-          </span>
-        </a>
-
-        <a href="#"> <!-- link to feels -->
-          <span class="section-item section-parent">
-            <div class="overlay-element white">
-              <div class="relative-wrapper">
-                <div class="img-wrapper">
-                  <img src="<?php echo get_template_directory_uri() . '/assets/images/main-menu/feels.jpg'?>" alt="the feels">
-                </div>
-                <div class="header-wrapper white">
-                  <p class="navigation--header small"><span class="pre">The</span><br />feels</p>
-                </div>
-              </div>
-            </div>
-          </span>
-        </a>
-
-        <a href="#"> <!-- link to dictionary -->
-          <span class="section-item section-parent">
-            <div class="overlay-element white">
-              <div class="relative-wrapper">
-                <div class="img-wrapper">
-                  <img src="<?php echo get_template_directory_uri() . '/assets/images/main-menu/dictionary.jpg'?>" alt="the dictionary">
-                </div>
-                <div class="header-wrapper white">
-                  <p class="navigation--header small"><span class="pre">The</span><br />dictionary</p>
-                </div>
-              </div>
-            </div>
-          </span>
-        </a>
-
-        <a href="#"> <!-- link to voyages -->
-          <span class="section-item section-parent">
-            <div class="overlay-element white">
-              <div class="relative-wrapper">
-                <div class="img-wrapper">
-                  <img src="<?php echo get_template_directory_uri() . '/assets/images/main-menu/voyages.jpg'?>" alt="the voyages">
-                </div>
-                <div class="header-wrapper white">
-                  <p class="navigation--header small"><span class="pre no-space">The</span><br />voyages</p>
-                </div>
-              </div>
-            </div>
-          </span>
-        </a>
-
-      </div>
-
-    </div>
-
-  </nav>
--- source/scripts/site/components/main-menu-component.js
+++ source/scripts/site/components/main-menu-component.js
@@ -16,8 +16,11 @@ site.components.MainMenuComponent = el.core.utils.class.extend(function(options)
   this.$el = this.options.$el;
   this.$mainMenuBar = this.$el.find('.main-menu-bar');
   this.menuOpened = false;
+  this.menuEnable = true;
 
   this._register();
+
+  this._init();
 
   console.log(this.name, this.options);
 
@@ -26,6 +29,25 @@ site.components.MainMenuComponent = el.core.utils.class.extend(function(options)
 
 }, site.components.BaseComponent);
 
+
+site.components.MainMenuComponent.prototype._init = function(data) {
+
+  var pageView = this.$el.data('page-view');
+
+  switch(pageView) {
+
+    case 'home':
+        this.menuEnable = false;
+        this.$el.addClass('disabled');
+        break;
+
+    case 'feeling-collectors':
+        this.manuEnable = false;
+        this.$el.addClass('basic').find('.vertical-button.go-home').removeClass('inverted');// Only display go-home btn
+
+  }
+
+}
 
 site.components.MainMenuComponent.prototype._resizeMainMenu = function(data) {
 
@@ -36,19 +58,23 @@ site.components.MainMenuComponent.prototype._resizeMainMenu = function(data) {
 
 site.components.MainMenuComponent.prototype._triggerMenuPosition = function(e) {
 
-	that = this;
 
-	if (this.menuOpened) {
-    this.$el.removeClass('open');
-		this.$mainMenuBar.removeClass('active').find('.hamburger-icon').removeClass('open');
-		this.menuOpened = false;
+  if ( this.menuEnable ) {
 
-	} else {
-    this.$el.addClass('open');
-		this.$mainMenuBar.addClass('active').find('.hamburger-icon').addClass('open');
-		this.menuOpened = true;
+    if (this.menuOpened) {
+      this.$el.removeClass('open');
+      this.$mainMenuBar.removeClass('active').find('.hamburger-icon').removeClass('open');
+      this.menuOpened = false;
 
-	}
+    } else {
+      this.$el.addClass('open');
+      this.$mainMenuBar.addClass('active').find('.hamburger-icon').addClass('open');
+      this.menuOpened = true;
+
+    }
+
+  }
+
 
 }
 
